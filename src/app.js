c178d73c688af5e53570a3e6e61bde6e2d2bf885
carlitossssss <gkggodjihh@gmail.com>
2024-08-08 03:48:23 -0400

    feat: Agregar soporte para carga perezosa de imágenes en la página de inicio

--- src/app.js
+++ src/app.js
@@ -1,25 +1,31 @@
 import express from 'express';
 import { config } from 'dotenv';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import authRoutes from './routes/authRoutes.js';
 import { dbMiddleware } from './middleware/dbMiddleware.js';
-import setupSwagger from './config/swagger.js'; // Importar configuración de Swagger
-
-config(); // Cargar variables de entorno
+import setupSwagger from './config/swagger.js'; 
+config();
 
 const app = express();
 
-app.use(cors()); // Habilitar CORS para todos los orígenes
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(cors()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, '../public')));
+
 app.get('/', (req, res) => {
-  res.send('Welcome to my API');
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.use(dbMiddleware); // Usar el middleware de conexión a la base de datos
+app.use(dbMiddleware); 
 app.use('/api/auth', authRoutes);
 
-setupSwagger(app); // Configurar Swagger
+setupSwagger(app);
 
 export default app;
